d6c838ba4596b6975f2d67c445967e2f6411e256
Anders Jenbo <anders@jenbo.dk>
2021-01-19 20:52:27 +0100

    Fix PHP 5.6 support

--- build.sh
+++ build.sh
@@ -1,9 +1,24 @@
 #!/usr/bin/env bash
-if command -v dpkg-query -l zip
+if ! command -v zip
 then
-  mkdir dist
-  composer install --no-dev -o
-  zip dist/altapay-for-woocommerce.zip -r * -x "dist/*" "tests/*" "bin/*" build.sh guide.md .gitignore phpunit.xml.dist phpstan.neon.dist composer.json composer.lock @
-else
   echo "Zip package is not currently installed"
+  exit
 fi
+
+if ! command -v php5.6
+then
+  echo "PHP 5.6 package is not currently installed"
+  exit
+fi
+
+if ! command -v composer
+then
+  echo "Composer package is not currently installed"
+  exit
+fi
+
+mkdir dist
+rm -rf vendor
+php5.6 $(composer) install --no-dev -o
+zip dist/altapay-for-woocommerce.zip -r * -x "dist/*" "tests/*" "bin/*" build.sh guide.md .gitignore phpunit.xml.dist phpstan.neon.dist composer.json composer.lock @
+composer install
