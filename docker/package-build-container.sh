d861b3241a0fe886755b9bed47081601b221daed
emicha <emicha@protonmail.com>
2021-01-28 10:14:37 +0100

    Remove duplicated script

--- docker/package-build-container.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker build ../ --file Dockerfile-build-image -t plugin-wordpress-package-build
-docker run --mount type=bind,source="$(pwd)/..",target=/app plugin-wordpress-package-build /bin/bash -c 'cd /app && bash build.sh'
