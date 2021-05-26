52e35ebdeb9451fb8774d42ce20de2a84abbaaff
Muhammad Idrees <saadidrees57@gmail.com>
2021-05-26 16:01:50 +0500

    Guest checkout allowed

--- Test/Integration test/cypress/integration/PageObjects/objects.js
+++ Test/Integration test/cypress/integration/PageObjects/objects.js
@@ -9,24 +9,14 @@ class Order
     {
         cy.fixture('config').then((url)=>{
         cy.visit(url.url) 
-        cy.get('.nav-menu > .page-item-9 > a').click()   
+   
             })    
     }
 
-    signin(){
-        cy.fixture('config').then((signin)=>{
-        cy.get('#username').type(signin.usrname)
-        cy.get('#password').type(signin.pass)
-        cy.get('.woocommerce-button').click()
-        cy.wait(1000)
-        cy.get('.beta > a').click().wait(1000)
-        cy.wait(1000)
-        })
-    }
     
     addproduct(){
         cy.get('.wp-block-product-new > .wc-block-grid__products > :nth-child(5) > .wc-block-grid__product-link > .wc-block-grid__product-image > .attachment-woocommerce_thumbnail').click()
-        cy.xpath('/html/body/div[2]/div[2]/div/div[2]/main/div[2]/div[2]/form/div/input').click().clear().type('3')
+        cy.xpath('/html/body/div[1]/div[2]/div/div[2]/main/div[2]/div[2]/form/div/input').click().clear().type('3')
         cy.get('.single_add_to_cart_button').click()
         cy.get('.woocommerce-message > .button').click()
         cy.get('.checkout-button').click()
@@ -35,7 +25,15 @@ class Order
 
     cc_payment(){
         cy.get('.wc_payment_method.payment_method_altapay_embraceit_test_terminal > label').click({force: true})
-        cy.xpath('/html/body/div[2]/div[2]/div/div[1]/main/article/div[1]/div/form[2]/div[2]/div/div/button').click({force: true})
+        //billing details
+        cy.get('#billing_first_name').type('Testperson-dk')
+        cy.get('#billing_last_name').type('Testperson-dk')
+        cy.get('#billing_address_1').type('Sæffleberggate 56,1 mf')
+        cy.get('#billing_postcode').type('6800')
+        cy.get('#billing_city').type('Varde')
+        cy.get('#billing_phone').type('20123456')
+        cy.get('#billing_email').type('demo@example.com')
+        cy.get('#place_order').click()
         cy.get('[id=creditCardNumberInput]').type('4111111111111111')
         cy.get('#emonth').type('01')
         cy.get('#eyear').type('2023')
@@ -58,13 +56,33 @@ class Order
     klarna_payment(){
 
         cy.get('.payment_method_altapay_embraceit_klarna_dkk_test_terminal > label').click({force: true}).wait(1000)
-        cy.get('#place_order').click().wait(3000)
-        cy.wait(3000)
-        cy.get('#submitbutton').click().wait(10000)
+        cy.get('#billing_first_name').type('Testperson-dk')
+        cy.get('#billing_last_name').type('Testperson-dk')
+        cy.get('#billing_address_1').type('Sæffleberggate 56,1 mf')
+        cy.get('#billing_postcode').type('6800')
+        cy.get('#billing_city').type('Varde')
+        cy.get('#billing_phone').type('20123456')
+        cy.get('#billing_email').type('demo@example.com')
+        cy.get('#place_order').wait(3000).click()
+        cy.get('#submitbutton').click().wait(8000)
 
+        cy.get('[id=klarna-pay-later-fullscreen]').then(($a) => { 
+            if ($a.find('[id=klarna-pay-later-fullscreen]').length) {
+                cy.get('[id=klarna-pay-later-fullscreen]').wait(3000)
+                    
+
+            } 
+
+            else {
+                  
+                cy.get('#submitbutton').click().wait(8000)
     
+            }
+        })
 
-        cy.get('[id=klarna-pay-later-fullscreen]').wait(2000).then(function($iFrame){
+        
+        
+        cy.get('[id=klarna-pay-later-fullscreen]').wait(3000).then(function($iFrame){
             const mobileNum = $iFrame.contents().find('[id=invoice_kp-purchase-approval-form-phone-number]')
             cy.wrap(mobileNum).type('(452) 012-3456')
             const personalNum = $iFrame.contents().find('[id=invoice_kp-purchase-approval-form-national-identification-number]')
@@ -84,7 +102,7 @@ class Order
     {
             cy.clearCookies()
             cy.fixture('config').then((admin)=>{
-            cy.visit(admin.url_admin_url)
+            cy.visit(admin.admin_url)
             cy.get('#user_login').clear().type(admin.wp_admin_user)
             cy.get('#user_pass').type(admin.wp_admin_pass)
             cy.get('#wp-submit').wait(1000).click()
