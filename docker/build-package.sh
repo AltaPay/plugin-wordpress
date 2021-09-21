6e6db13ce85584bd97cdbef25bbb974334e62d6d
Shahbaz Anjum <shahbaz.mehar@outlook.com>
2021-09-21 11:51:20 +0500

    Update docker script to run from root directory

--- docker/build-package.sh
+++ docker/build-package.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build ../ --file Dockerfile-build-image -t plugin-wordpress-package-build
-docker run --rm --mount type=bind,source="$(pwd)/..",target=/app plugin-wordpress-package-build /bin/bash -c 'cd /app && bash build.sh'
+docker build . --file docker/Dockerfile-build-image -t plugin-wordpress-package-build
+docker run --rm --mount type=bind,source="$(pwd)",target=/app plugin-wordpress-package-build ../bin/bash -c 'cd /app && bash build.sh'
