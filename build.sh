c4b7a67306a8051f722565022f3eb8a34e67d353
Shahbaz Anjum <shahbaz.mehar@outlook.com>
2022-01-04 12:24:41 +0500

    Exclude docs and wiki.md from build package

--- build.sh
+++ build.sh
@@ -26,5 +26,5 @@ fi
 mkdir -p dist
 rm -rf vendor
 php7.0 $(command -v composer) install --no-dev -o
-zip dist/altapay-for-woocommerce.zip -r * -x "dist/*" "tests/*" "bin/*" "terminal-config/*" build.sh README.md guide.md .gitignore phpunit.xml.dist phpstan.neon.dist composer.json composer.lock @
+zip dist/altapay-for-woocommerce.zip -r * -x "dist/*" "tests/*" "bin/*" "terminal-config/*" "docs/*" wiki.md build.sh README.md guide.md .gitignore phpunit.xml.dist phpstan.neon.dist composer.json composer.lock @
 composer install
