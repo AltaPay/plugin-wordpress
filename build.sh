9a30529e45efa0238c2cd8781efca0910258a561
Mahad Ashraf <>
2022-02-25 17:50:30 +0500

    update the build & docker-build script to take php version as parameter, update  docker-build script to support php v7.0 & v8.1 only

--- build.sh
+++ build.sh
@@ -1,30 +1,39 @@
 #!/usr/bin/env bash
 
+if [ -z "$1" ]
+  then
+    echo "No PHP version supplied"
+    echo "Usage: ./build.sh <php_version>"
+    echo "./build.sh 7.0"
+    echo "./build.sh 8.1"
+    exit
+fi
+
 set -e
 
 if ! command -v zip
-then	
+then
   echo "Zip package is not currently installed"
-  echo "Or use docker/build-package.sh"
+  echo "Or use './docker/build-package.sh <php_version>'"
   exit
 fi
 
-if ! command -v php7.0
+if ! command -v php$1
 then
-  echo "PHP 7.0 package is not currently installed"
-  echo "Or use docker/build-package.sh"
+  echo "PHP $1 package is not currently installed"
+  echo "Or use './docker/build-package.sh <php_version>'"
   exit
 fi
 
 if ! command -v composer
 then
   echo "Composer package is not currently installed"
-  echo "Or use docker/build-package.sh"
+  echo "Or use './docker/build-package.sh <php_version>'"
   exit
 fi
 
 mkdir -p dist
 rm -rf vendor
-php7.0 $(command -v composer) install --no-dev -o --no-interaction
+php$1 $(command -v composer) install --no-dev -o --no-interaction
 zip dist/altapay-for-woocommerce.zip -r * -x "dist/*" "tests/*" "bin/*" "terminal-config/*" "docs/*" wiki.md build.sh README.md guide.md .gitignore phpunit.xml.dist phpstan.neon.dist composer.json composer.lock @
 composer install --no-interaction
--- docker/build-package.sh
+++ docker/build-package.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 
-docker build . --file docker/Dockerfile-build-image -t plugin-wordpress-package-build
-docker run --rm --mount type=bind,source="$(pwd)",target=/app plugin-wordpress-package-build ../bin/bash -c 'cd /app && bash build.sh'
+supported_versions_msg="Only PHP versions 7.0 & 8.1 are supported"
+
+if [ -z "$1" ]
+  then
+    echo "No PHP version supplied"
+    echo "$supported_versions_msg"
+    echo "Usage: ./build-package.sh <php_version>"
+    echo "./build-package.sh 7.0"
+    echo "./build-package.sh 8.1"
+    exit 1
+fi
+
+if [ "$1" == "7.0" ]
+  then
+    docker build . --file docker/Dockerfile-build-image-php-7.0 -t plugin-wordpress-package-build
+elif [ "$1" == "8.1" ]
+  then
+    docker build . --file docker/Dockerfile-build-image-php-8.1 -t plugin-wordpress-package-build
+else
+  echo "$supported_versions_msg"
+  exit 1
+fi
+
+docker run --rm --mount type=bind,source="$(pwd)",target=/app plugin-wordpress-package-build ../bin/bash -c 'cd /app && bash build.sh '$1
