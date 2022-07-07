233f4da9f65c8cbdff07d2dcfad4661a6f6724e4
Muhammad Idrees <saadidrees57@gmail.com>
2022-07-07 16:03:07 +0500

    testing klarna falure

--- tests/integration-test/cypress/integration/PageObjects/objects.js
+++ tests/integration-test/cypress/integration/PageObjects/objects.js
@@ -60,7 +60,7 @@ class Order {
         cy.get('#billing_phone').clear().type('33 13 71 12')
         cy.get('#billing_email').clear().type('customer@email.dk')
         cy.get('#place_order').click()
-        cy.get('#submitbutton').click().wait(8000)
+        cy.get('#submitbutton').click().wait(10000)
         cy.get('[id=submitbutton]').click().wait(3000)
         cy.get('[id=klarna-pay-later-fullscreen]').wait(4000).then(function($iFrame){
             const mobileNum = $iFrame.contents().find('[id=email_or_phone]')
