6f1cb85bb588b198c3d20a8f4d0b36ca6b75a0f3
Shahbaz <shahbaz.mehar@outlook.com>
2023-10-24 15:41:43 +0500

    Update plugin and wc tested upto version

--- altapay.php
+++ altapay.php
@@ -5,10 +5,10 @@
  * Description: Payment Gateway to use with WordPress WooCommerce
  * Author: AltaPay
  * Author URI: https://altapay.com
- * Version: 3.4.9
+ * Version: 3.5.0
  * Name: SDM_Altapay
  * WC requires at least: 3.9.0
- * WC tested up to: 8.1.1
+ * WC tested up to: 8.2.1
  *
  * @package Altapay
  */
