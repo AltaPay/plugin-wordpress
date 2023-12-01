380d3e0bd643f76731106678e81e0c92851f26fa
Shahbaz <shahbaz.mehar@outlook.com>
2023-12-01 10:16:57 +0500

    Fix duplicate transactions issue with WPML plugin

--- assets/js/applepay.js
+++ assets/js/applepay.js
@@ -1,4 +1,110 @@
-function onApplePayButtonClicked(applepay_obj) {
+jQuery(function ($) {
+
+	let session = '';
+
+	if ( typeof altapay_applepay_obj === 'undefined' ) {
+		return false;
+	}
+
+	jQuery('form.checkout').on('checkout_place_order_' + altapay_applepay_obj.applepay_payment_method, function (event, wc_checkout_form) {
+			var $form = $( 'form.checkout' );
+			$form.addClass( 'processing' );
+
+			wc_checkout_form.blockOnSubmit( $form );
+
+			// Attach event to block reloading the page when the form has been submitted
+			wc_checkout_form.attachUnloadEventsOnSubmit();
+
+			// ajaxSetup is global, but we use it to ensure JSON is valid once returned.
+			$.ajaxSetup( {
+				dataFilter: function( raw_response, dataType ) {
+					// We only want to work with JSON
+					if ( 'json' !== dataType ) {
+						return raw_response;
+					}
+
+					if ( wc_checkout_form.is_valid_json( raw_response ) ) {
+						return raw_response;
+					} else {
+						// Attempt to fix the malformed JSON
+						var maybe_valid_json = raw_response.match( /{"result.*}/ );
+
+						if ( null === maybe_valid_json ) {
+							console.log( 'Unable to fix malformed JSON' );
+						} else if ( wc_checkout_form.is_valid_json( maybe_valid_json[0] ) ) {
+							console.log( 'Fixed malformed JSON. Original:' );
+							console.log( raw_response );
+							raw_response = maybe_valid_json[0];
+						} else {
+							console.log( 'Unable to fix malformed JSON' );
+						}
+					}
+
+					return raw_response;
+				}
+			} );
+
+			$.ajax({
+				type:		'POST',
+				url:		wc_checkout_params.checkout_url,
+				data:		$form.serialize(),
+				dataType:   'json',
+				success:	function( result ) {
+					// Detach the unload handler that prevents a reload / redirect
+					wc_checkout_form.detachUnloadEventsOnSubmit();
+
+					try {
+						if ( 'success' === result.result && $form.triggerHandler( 'checkout_place_order_success', [ result, wc_checkout_form ] ) !== false ) {
+							onApplePayButtonClicked( altapay_applepay_obj, false, wc_checkout_form, result.order_id );
+						} else if ( 'failure' === result.result ) {
+							console.log( 'failure' );
+							throw 'Result failure';
+						} else {
+							throw 'Invalid response';
+						}
+					} catch( err ) {
+						// Reload page
+						if ( true === result.reload ) {
+							window.location.reload();
+							return;
+						}
+
+						// Trigger update in case we need a fresh nonce
+						if ( true === result.refresh ) {
+							$( document.body ).trigger( 'update_checkout' );
+						}
+
+						// Add new errors
+						if ( result.messages ) {
+							wc_checkout_form.submit_error( result.messages );
+						} else {
+							wc_checkout_form.submit_error( '<div class="woocommerce-error">' + wc_checkout_params.i18n_checkout_error + '</div>' ); // eslint-disable-line max-len
+						}
+					}
+				},
+				error: function( jqXHR, textStatus, errorThrown ) {
+					// Detach the unload handler that prevents a reload / redirect
+					wc_checkout_form.detachUnloadEventsOnSubmit();
+
+					wc_checkout_form.submit_error(
+						'<div class="woocommerce-error">' +
+						( errorThrown || wc_checkout_params.i18n_checkout_error ) +
+						'</div>'
+					);
+				}
+			});
+
+			return false;
+	});
+
+	jQuery(document).on("click", "#place_order", function(){
+		if ($('#payment_method_' + altapay_applepay_obj.applepay_payment_method).is(':checked')) {
+			onApplePayButtonClicked(altapay_applepay_obj, true, false);
+		}
+	});
+});
+
+function onApplePayButtonClicked(applepay_obj, createSession, wc_checkout_form, order_id) {
 
 	if ( ! ApplePaySession) {
 		return;
@@ -20,9 +126,11 @@ function onApplePayButtonClicked(applepay_obj) {
 	};
 
 	// Create ApplePaySession
-	const session = new ApplePaySession( 3, request );
+	if (createSession) {
+		session = new ApplePaySession( 3, request );
+	}
 
-	session.onvalidatemerchant = async event => {
+	session.onvalidatemerchant = event => {
 		jQuery.post(
 			applepay_obj.ajax_url,
 			{
@@ -76,7 +184,7 @@ function onApplePayButtonClicked(applepay_obj) {
 				action: 'card_wallet_authorize',
 				provider_data: JSON.stringify( event.payment.token ),
 				terminal: applepay_obj.terminal,
-				order_id: applepay_obj.order_id
+				order_id: order_id
 			},
 			function (res) {
 				let status;
@@ -87,6 +195,8 @@ function onApplePayButtonClicked(applepay_obj) {
 					status = ApplePaySession.STATUS_FAILURE;
 					session.completePayment( status );
 				}
+
+				wc_checkout_form.$checkout_form.removeClass( 'processing' ).unblock();
 
 				if (res.data.redirect) {
 					window.location = res.data.redirect;
@@ -114,7 +224,12 @@ function onApplePayButtonClicked(applepay_obj) {
 
 	session.oncancel = event => {
 		// Payment cancelled by WebKit
+		if(wc_checkout_form){
+			wc_checkout_form.$checkout_form.removeClass( 'processing' ).unblock();
+		}
 	};
 
-	session.begin();
+	if (!createSession) {
+		session.begin();
+	}
 }
