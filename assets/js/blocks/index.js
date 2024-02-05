51b015e6f784afab087b8c1174134b3ff1eadc77
Shahbaz <shahbaz.mehar@outlook.com>
2024-02-05 17:21:38 +0500

    Add script file to extend checkout blocks

---
+++ assets/js/blocks/index.js
@@ -0,0 +1,59 @@
+import { sprintf, __ } from '@wordpress/i18n';
+import { registerPaymentMethod } from '@woocommerce/blocks-registry';
+import { decodeEntities } from '@wordpress/html-entities';
+import { getSetting } from '@woocommerce/settings';
+
+const terminal_key = '';
+const terminal_name = '';
+
+const settings = getSetting( terminal_key + '_data', {} );
+
+const defaultLabel = __(
+	terminal_name,
+	'woo-gutenberg-products-block'
+);
+
+const label = decodeEntities( settings.title ) || defaultLabel;
+
+/**
+ * Content component
+ */
+const Content = () => {
+	return decodeEntities( settings.description || '' );
+};
+
+
+/**
+ * Label component
+ *
+ * @param {*} props Props from payment API.
+ */
+const Label = ( props ) => {
+	const { PaymentMethodLabel } = props.components;
+	return <PaymentMethodLabel text={ label } />;
+};
+
+/**
+ * AltaPay payment method config object.
+ */
+const altapayPaymentMethod = {
+	name: terminal_key,
+	label: (
+		<>
+			<span class='altapay-payment-method'>
+				{ __( label, 'woocommerce-payments' ) }
+					<img src={ settings.icon }
+				alt={ settings.icon } />
+			</span>
+		</>
+	),
+	content: <Content />,
+	edit: <Content />,
+	canMakePayment: () => true,
+	ariaLabel: label,
+	supports: {
+		features: settings.supports,
+	},
+};
+
+registerPaymentMethod( altapayPaymentMethod );
