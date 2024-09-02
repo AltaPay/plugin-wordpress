f7f2b1996f906179a3823ab29e1a9a64b2d41559
Shahbaz <shahbaz.mehar@outlook.com>
2024-09-02 15:45:49 +0500

    Fix subscription products issues

--- classes/util/UtilMethods.php
+++ classes/util/UtilMethods.php
@@ -21,13 +21,13 @@ class UtilMethods {
 	 * @param WC_Order|WC_Order_Refund $order
 	 * @param array                    $products
 	 * @param bool                     $wcRefund
+	 *
 	 * @return array  list of order lines
 	 */
 	public function createOrderLines( $order, $products = array(), $wcRefund = false, $isSubscription = false ) {
-		$orderlineDetails         = array();
-		$itemsToCapture           = array();
-		$couponDiscountPercentage = 0; // set initial coupon discount to 0
-		$cartItems                = $order->get_items(); // get cart items
+		$orderLines     = array();
+		$itemsToCapture = array();
+		$cartItems      = $order->get_items();
 
 		// If capture request is triggered
 		if ( $products ) {
@@ -48,34 +48,38 @@ class UtilMethods {
 
 		// generate order lines product by product
 		foreach ( $cartItems as $key => $item ) {
+			$total    = $item->get_total();
+			$subtotal = $item->get_subtotal();
+			$discount = 0;
 
-			if ( $item->get_total() == 0 ) {
+			if ( $total == 0 ) {
 				continue;
 			}
 
-			$appliedCouponItems = $order->get_items( 'coupon' ); // get items with coupon discount
-			if ( $appliedCouponItems ) {
-				$couponDiscountPercentage = $this->getCouponDiscount( $appliedCouponItems, $item );
+			$discountPercentage = 0;
+			if ( $subtotal > 0 ) {
+				$discount           = $subtotal - $total;
+				$discountPercentage = ( $subtotal - $total ) / $subtotal * 100;
 			}
 			// get product details for each order line
-			$productDetails = $this->getProductDetails( $item, $couponDiscountPercentage, ( ( $i == 0 ) ? $isSubscription : 0 ) );
+			$productDetails = $this->getProductDetails( $item, $discount, $discountPercentage, ( ( $i == 0 ) ? $isSubscription : 0 ) );
 
 			if ( $wcRefund ) {
-				$orderlineDetails[ $key ] = array(
+				$orderLines[ $key ] = array(
 					'qty'          => $products['itemQty'][ $key ],
 					'refund_total' => ( $item['line_total'] / $item->get_quantity() ) * $products['itemQty'][ $key ],
 					'refund_tax'   => ( $item->get_total_tax() / $item->get_quantity() ) * $products['itemQty'][ $key ],
 				);
 			} else {
-				$orderlineDetails[] = $productDetails['product'];
+				$orderLines[] = $productDetails['product'];
 			}
 
 			// check if compensation exists to bind it in orderline details
 			if ( ! $wcRefund && isset( $productDetails['compensation'] ) ) {
-				$orderlineDetails [] = $productDetails['compensation'];
+				$orderLines [] = $productDetails['compensation'];
 			}
 
-			$i++;
+			$i ++;
 		}
 		// get the shipping Details
 		$shippingDetails = $this->getShippingDetails(
@@ -85,143 +89,90 @@ class UtilMethods {
 		);
 
 		if ( $shippingDetails ) {
-			$shippingDetails     = reset( $shippingDetails );
-			$orderlineDetails [] = $shippingDetails;
+			$shippingDetails = reset( $shippingDetails );
+			$orderLines []   = $shippingDetails;
 		}
-
-		return $orderlineDetails;
-	}
-
-	/**
-	 * Returns the total of multiple discounts applied on each order line
-	 *
-	 * @param array $appliedCouponItems
-	 * @param  array $orderline
-	 * @return float returns the float value of percentage discounts applied using coupon codes
-	 */
-	public function getCouponDiscount( $appliedCouponItems, $orderline ) {
-		$discountPercentageWholeCart           = 0;
-		$discountPercentageOnParticularProduct = 0;
-		$productsWithCoupon                    = array();
-
-		if ( $appliedCouponItems ) {
-			foreach ( $appliedCouponItems as $item_id => $item ) {
-				// Retrieving the coupon ID reference
-				$couponPostObj = get_page_by_title( $item->get_name(), OBJECT, 'shop_coupon' );
-				$couponID      = $couponPostObj->ID;
-				// Get an instance of WC_Coupon object (necessary to use WC_Coupon methods)
-				$coupon         = new WC_Coupon( $couponID );
-				$couponType     = $coupon->discount_type;
-				$appliedCoupons = reset( $coupon );
-				// Filtering with your coupon custom types
-				if ( $couponType === 'percent' && empty( $appliedCoupons['product_ids'] ) ) {
-					// Get the Coupon discount amounts in the order
-					$orderDiscountAmount    = wc_get_order_item_meta( $item_id, 'discount_amount', true );
-					$orderDiscountTaxAmount = wc_get_order_item_meta( $item_id, 'discount_amount_tax', true );
-					// This calculation will assist in scenario of discount coupons on entire cart
-					$totalCouponDiscountAmount = $orderDiscountAmount + $orderDiscountTaxAmount;
-					// Or get the coupon amount object
-					$discountPercentageWholeCart += $coupon->amount;
-				} elseif ( $couponType === 'percent' && ! empty( $appliedCoupons['product_ids'] ) ) {
-					$discountPercentageOnParticularProduct = $coupon->amount;
-					$productsWithCoupon                    = array_values( $appliedCoupons['product_ids'] );
-				}
-			}
-		}
-		if ( in_array( $orderline['product_id'], $productsWithCoupon ) || in_array(
-			$orderline['variation_id'],
-			$productsWithCoupon
-		) ) {
-			$discountPercentage = $discountPercentageWholeCart + $discountPercentageOnParticularProduct;
-		} else {
-			$discountPercentage = $discountPercentageWholeCart;
-		}
-
-		return $discountPercentage;
+		return $orderLines;
 	}
 
 	/**
 	 * Returns product Details based on product type and tax configuration settings
 	 *
-	 * @param object[] $item
-	 * @param float    $couponDiscountPercentage
+	 * @param object $item
+	 * @param float  $discount
+	 * @param float  $discountPercentage
+	 *
 	 * @return array
 	 */
-	private function getProductDetails( $item, $couponDiscountPercentage, $isSubscription = false ) {
-		$discountPercentage  = 0; // set discount Percent to 0 by default
-		$productCartId       = $item->get_id(); // product Cart ID number
-		$singleProduct       = wc_get_product( $item['product_id'] ); // Details of each product
-		$productQuantity     = $item['qty']; // get ordered number of quantity for each orderline
-		$productRegularPrice = $singleProduct->get_regular_price();
-		$productSalePrice    = $singleProduct->get_sale_price();
-		$productData         = array();
+	private function getProductDetails( $item, $discount, $discountPercentage, $isSubscription = false ) {
+		$product         = $item->get_product();
+		$quantity        = $item->get_quantity();
+		$regularPrice    = (float) $product->get_regular_price();
+		$salePrice       = (float) $product->get_sale_price();
+		$subtotal        = $item->get_subtotal();
+		$taxRate         = $subtotal > 0 ? $item->get_subtotal_tax() / $subtotal : 0;
+		$totalCMS        = $item->get_total() + $item->get_total_tax();
+		$productId       = $product->get_sku() ? $item->get_id() . '-' . $product->get_sku() : $item->get_id();
+		$productData     = array();
+		$productDiscount = 0;
 
-		// Get and set tax rate using order line
-		$orderlineTax = $item['subtotal'] > 0 ? array_sum( $item['taxes']['subtotal'] ) / $item['subtotal'] : 0;
-		$taxRate      = $orderlineTax;
-
-		// CMS total
-		$totalCMS = $item->get_total() + $item->get_total_tax();
-
-		// set product ID based on the sku provided
-		if ( $singleProduct->get_sku() ) {
-			$productId = $productCartId . '-' . $singleProduct->get_sku();
-		} else {
-			$productId = $productCartId;
-		}
-
-		// get and set product details in case of variable product
-		if ( $singleProduct->get_type() === 'variable' ) {
-			$variablePrices      = $singleProduct->get_variation_prices(); // get all the variation prices i.e regular and sale
-			$variationID         = $item['variation_id']; // get variation id of ordered orderline
-			$productRegularPrice = $variablePrices['regular_price'][ $variationID ]; // get regular price from variation prices array
-			$productSalePrice    = $variablePrices['sale_price'][ $variationID ]; // get regular price from variation prices array
+		if ( $product->get_type() === 'variable' || $product->get_type() === 'variable-subscription' ) {
+			$variationId  = (int) $item->get_variation_id();
+			$product_data = wc_get_product( $variationId );
+			$regularPrice = (float) $product_data->get_regular_price();
+			$salePrice    = (float) $product_data->get_sale_price() ? $product_data->get_sale_price() : 0;
 		}
 
 		// calculate discount if catalogue rule is applied on order line i.e. product sale price is set
-		if ( $singleProduct->is_on_sale() ) {
-			$productDiscountAmount = $productRegularPrice - $productSalePrice; // calculate discount amount
+		if ( $product->is_on_sale() ) {
+			$productDiscount = $regularPrice - $salePrice;
 			// convert discount amount into percentage
-			$discountPercentage = round( ( $productDiscountAmount / $productRegularPrice ) * 100, 2 );
-		}
-
-		if ( $couponDiscountPercentage > 0 ) {
-			$discountPercentage = $couponDiscountPercentage;
+			$discountPercentage += round( ( $productDiscount / $regularPrice ) * 100, 2 );
 		}
 
 		if ( wc_prices_include_tax() ) {
-			$taxRate      = 1 + $orderlineTax;
-			$productPrice = round( $productRegularPrice / $taxRate, 2 );
-			$taxAmount    = $productRegularPrice - $productPrice;
+			$taxRate      = 1 + $taxRate;
+			$productPrice = round( $regularPrice / $taxRate, 2 );
+			$taxAmount    = $regularPrice - $productPrice;
 		} else {
-			$productPrice = $productRegularPrice;
+			$productPrice = $regularPrice;
 			$taxAmount    = $productPrice * $taxRate;
 		}
 
 		// calculate total generated from order lines generated after calculation
-		$totalOrderlines = ( ( $productPrice + $taxAmount ) - ( ( $productPrice + $taxAmount ) * ( $discountPercentage / 100 ) ) ) * $productQuantity;
+		$orderLineTotal = ( ( $productPrice + $taxAmount ) - ( ( $productPrice + $taxAmount ) * ( $discountPercentage / 100 ) ) ) * $quantity;
 		// calculate compensation amount between total generated from woocommerce and total generated from orderlines
-		$compensationAmount = round( ( $totalCMS - $totalOrderlines ), 3 );
+		$compensationAmount = round( ( $totalCMS - $orderLineTotal ), 3 );
 		// generate compensation amount order line using product id and amount
 		if ( $compensationAmount > 0 || $compensationAmount < 0 ) {
 			$productData['compensation'] = $this->compensationAmountOrderline( $productId, $compensationAmount );
 		}
+
+		$productPrice = $isSubscription ? ( $productPrice + ( $taxAmount * $quantity ) - ( $discount + $productDiscount ) ) : round( $productPrice, 2 );
+		$taxPercent   = $productPrice > 0 ? round( ( $taxAmount / $productPrice ) * 100, 2 ) : 0;
+		$taxAmount    = round( $taxAmount * $quantity, 2 );
+		$discount     = round( $discountPercentage, 2 );
+
 		// generate line date with all the calculated parameters
-		$orderLine = new OrderLine(
-			$item['name'],
+		$orderLine             = new OrderLine(
+			$item->get_name(),
 			$productId,
-			$productQuantity,
+			$quantity,
 			round( $productPrice, 2 )
 		);
+		$orderLine->productUrl = $product->get_permalink();
+		$orderLine->imageUrl   = wp_get_attachment_url( $product->get_image_id() );
+		$orderLine->unitCode   = $quantity > 1 ? 'units' : 'unit';
 
-		$orderLine->discount   = round( $discountPercentage, 2 );
-		$orderLine->taxAmount  = round( $taxAmount * $productQuantity, 2 );
-		$orderLine->taxPercent = $productPrice > 0 ? round( ( $taxAmount / $productPrice ) * 100, 2 ) : 0;
-		$orderLine->productUrl = get_permalink( $singleProduct->get_id() );
-		$orderLine->imageUrl   = wp_get_attachment_url( get_post_thumbnail_id( $singleProduct->get_id() ) );
-		$orderLine->unitCode   = $productQuantity > 1 ? 'units' : 'unit';
-		$goodsType             = ( $isSubscription ) ? 'subscription_model' : 'item';
+		if ( ! $isSubscription ) {
+			$orderLine->discount   = $discount;
+			$orderLine->taxAmount  = $taxAmount;
+			$orderLine->taxPercent = $taxPercent;
+		}
+
+		$goodsType = ( $isSubscription ) ? 'subscription_model' : 'item';
 		$orderLine->setGoodsType( $goodsType );
+
 		$productData['product'] = $orderLine;
 
 		return $productData;
@@ -258,6 +209,7 @@ class UtilMethods {
 	 * @param WC_Order $order
 	 * @param array    $products
 	 * @param bool     $wcRefund
+	 *
 	 * @return array|bool
 	 */
 	private function getShippingDetails( $order, $products, $wcRefund ) {
@@ -302,6 +254,7 @@ class UtilMethods {
 				$shippingDetails[] = $orderLine;
 			}
 		}
+
 		return $shippingDetails;
 	}
 }
