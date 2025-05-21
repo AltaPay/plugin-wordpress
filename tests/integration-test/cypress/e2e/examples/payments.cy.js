7de51f45ec2e2dd9e4a2cb001ec4aac9d8890b67
TMansur5 <tma@technologies.dk>
2025-05-21 11:45:09 +0500

    Surcharge Integration Test

--- tests/integration-test/cypress/e2e/examples/payments.cy.js
+++ tests/integration-test/cypress/e2e/examples/payments.cy.js
@@ -291,6 +291,42 @@ describe('WooCommerce', function () {
         })
     })
 
+    it('Surcharge Payment', function () {
+
+        const ord = new Order()
+        ord.clrcookies()
+        ord.visit()
+        cy.get('body').then(($body) => {
+            if ($body.text().includes('€')) {
+                ord.admin()
+                ord.change_currency_to_DKK()
+                ord.visit()
+            }
+        })
+        ord.addproduct()
+        cy.fixture('config').then((admin) => {
+            if (admin.CC_TERMINAL_NAME != "") {
+                cy.get('body').then(($a) => {
+                    if ($a.find("label:contains('" + admin.CC_TERMINAL_NAME + "')").length) {
+                        ord.surcharge_payment(admin.CC_TERMINAL_NAME)
+                        ord.admin()
+                        ord.capture()
+                        ord.refund()
+                    } else {
+                        cy.log(admin.CC_TERMINAL_NAME + ' not found in page')
+                        this.skip()
+                    }
+
+                })
+
+            }
+            else {
+                cy.log('CC_TERMINAL_NAME skipped')
+                this.skip()
+            }
+        })
+    })
+
     it('iDEAL Payment', function () {
         const ord = new Order()
         ord.visit()
