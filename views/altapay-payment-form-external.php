844c71fa76770587ed071c13a8746ea18cd9496f
Shahbaz <shahbaz.mehar@outlook.com>
2025-10-15 18:50:28 +0500

    Add copyright information

--- views/altapay-payment-form-external.php
+++ views/altapay-payment-form-external.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * AltaPay module for WooCommerce
+ *
+ * Copyright © 2025 AltaPay. All rights reserved.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 defined( 'ABSPATH' ) || exit;
 ?>
 <!doctype html>
